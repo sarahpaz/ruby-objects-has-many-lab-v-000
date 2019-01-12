e84dc2733f3916f76890d10e73aaa0557bfb9a68
Sarah Paz <sarah.paz@gmail.com>
2019-01-12 05:41:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
@@ -15,8 +15,13 @@ class Artist
     @songs
   end
 
-  def add_song_by_name(title)
-    song = Song.new(title)
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
     song.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,15 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
-    @@all = []
-    @@all
+    @@all << self
   end
 
   def self.all
-    @@all 
+    @@all
   end
 
   def artist_name
