f16c7fa8716234ed36362d72092aa6d6dd81241e
Sarah Paz <sarah.paz@gmail.com>
2019-01-30 13:58:13 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -20,44 +20,8 @@ class Author
     @posts << post
     post.author = self
   end
+
+  def self.post_count
+    Post.all.count
+  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Author
-#   attr_accessor :name
-#
-#   def initialize(name)
-#     @name = name
-#     @posts = []
-#   end
-#
-#   def add_post(post)
-#     @posts << post
-#     post.author = self
-#   end
-#
-#   def posts
-#     @posts
-#   end
-#
-#   def add_post_by_title(title)
-#     post = Post.new(title)
-#     @posts << post
-#     post.author = self
-#   end
-#
-#   def self.post_count
-#     Post.all.count
-#   end
-# end
--- lib/post.rb
+++ lib/post.rb
@@ -7,41 +7,13 @@ class Post
     @title = title
     @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    
+    end
+  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Post
-#   attr_accessor :title, :author
-#
-#   @@all = []
-#
-#   def initialize(title)
-#     @title = title
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def author_name
-#     if self.author == nil
-#       nil
-#     else
-#       self.author.name
-#     end
-#   end
-# end
